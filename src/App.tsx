9e531abe82361960b026e3c303f1509474d1266b
Alexander Kachan <akach.dev@mail.ru>
2023-08-13 20:34:27 +0300

    add grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
-
-
-  render() {
-    return (
-       <div className="App">
-         Hello, samurai! Let's go!
-       </div>
-    );
-  }
+function App() {
+  return (
+     <div className={'wrapper'}>
+       <header>header</header>
+       <aside>aside</aside>
+       <main>main</main>
+       <footer>footer</footer>
+     </div>
+  );
 }
 
 export default App;
