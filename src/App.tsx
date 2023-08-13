efdcbfa13d92f85476031300b8fc6b860aa93b2a
Alexander Kachan <akach.dev@mail.ru>
2023-08-13 20:56:17 +0300

    11 lesson done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
+import img from '../src/assets/img/img.webp'
 
 function App() {
   return (
      <div className={'wrapper'}>
        <header>header</header>
-       <aside>aside</aside>
-       <main>main</main>
+       <aside>
+         <nav>
+           <ul>
+             <li>Profile</li>
+             <li>Messages</li>
+             <li>News</li>
+             <li>Music</li>
+           </ul>
+         </nav>
+         <span>Settings</span>
+       </aside>
+       <main>
+         <img
+            className={'img'}
+            src={img} alt=""/>
+       </main>
        <footer>footer</footer>
      </div>
   );
