b458f30ce5a6efd180a52c50afd12114aa60c051
Alexander Kachan <akach.dev@mail.ru>
2023-08-15 20:50:05 +0300

    lesson 17 done

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React from 'react';
 import s from './Profile.module.css'
 
 import house from "../../assets/img/img.webp";
@@ -6,14 +6,14 @@ import {MyPosts} from "./myPosts/MyPosts";
 
 type ProfilePropsType = {}
 
-export const Profile: FC<ProfilePropsType> = (props) => {
+export function Profile() {
   return (
      <div>
        <img className={s.img} src={house} alt="house"/>
-       
+
        <MyPosts/>
      </div>
   );
-};
+}
 
 
