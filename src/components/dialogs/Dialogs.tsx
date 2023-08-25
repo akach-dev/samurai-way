1c1da2ac9a304afe1ccfab558df427870569a86a
Alexander Kachan <akach.dev@mail.ru>
2023-08-25 19:48:35 +0300

    lesson 22 done

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
   return (
      <div className={s.dialogs}>
        <ul className={s.dialogsItems}>
-         <li className={s.dialogsItem}>Dimcho</li>
-         <li className={s.dialogsItem}>Andrey</li>
-         <li className={s.dialogsItem}>Svea</li>
-         <li className={s.dialogsItem}>Victor</li>
-         <li className={s.dialogsItem}>Valera</li>
+         <li className={s.dialogsItem}>
+           <NavLink to='/dialogs/1'>Dimcho</NavLink>
+         </li>
+         <li className={s.dialogsItem}>
+           <NavLink to='/dialogs/2'>Andrey</NavLink>
+         </li>
+         <li className={s.dialogsItem}>
+           <NavLink to='/dialogs/3'>Svea</NavLink>
+         </li>
+         <li className={s.dialogsItem}>
+           <NavLink to='/dialogs/4'>Victor</NavLink>
+         </li>
+         <li className={s.dialogsItem}>
+           <NavLink to='/dialogs/5'>Valera</NavLink>
+         </li>
        </ul>
        <ul className={s.messages}>
          <li className={s.messagesItem}>Lorem ipsum dolor sit amet, consecrate radicalising elit.</li>
