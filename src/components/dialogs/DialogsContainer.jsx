7afe19bf259c7ac2f782bff4a3f45646e910a0c5
Alexander Kachan <akach.dev@mail.ru>
2023-10-01 19:23:20 +0300

    lesson 43 done

--- src/components/dialogs/DialogsContainer.jsx
+++ src/components/dialogs/DialogsContainer.jsx
@@ -5,15 +5,13 @@ import {Dialogs} from "./Dialogs";
 
 
 export const DialogsContainer = ({store}) => {
-  const newMessageText = store.getState().messagesPage.newMessageText
 
+  const newMessageText = store.getState().messagesPage.newMessageText
   const dialog = store.getState().messagesPage.dialogs.map(dialog => <DialogsItem key={dialog.id} name={dialog.name}
                                                                                   id={dialog.id}/>)
   const message = store.getState().messagesPage.messages.map(message => <Message key={message.id}
                                                                                  message={message.message}/>)
-
   const onSendMessage = () => store.dispatch(sendNewMessageTextAC())
-
   const onNewMessage = (text) => {
     store.dispatch(updateNewMessageTextAC(text))
   };
