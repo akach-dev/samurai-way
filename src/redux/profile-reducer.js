2e8e1fc600a4c91b0c90720c75f0b9102b76c298
Alexander Kachan <akach.dev@mail.ru>
2023-10-04 21:00:19 +0300

    lesson 48 done

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -14,10 +14,7 @@ export const profileReducer = (state = initialState, action) => {
       let newPost = {
         id: '5', message: state.newPostText, likesCount: 0
       }
-
       return {...state, posts: [...state.posts, newPost], newPostText: ''}
-
-
     case "UPDATE-NEW-POST-TEXT":
       return {...state, newPostText: action.text}
 
